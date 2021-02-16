a7a1e59704edfe71b63cd87656a8ba574e8f87e7
Moon-HyeKyung <ansgprud02@gmail.com>
2021-02-16 17:52:35 +0900

    First_Commit

---
+++ talent_shop/talent_shop/src/main/java/com/example/talent_shop/TalentShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.talent_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentShopApplication.class, args);
+	}
+
+}
---
+++ talent_shop/talent_shop/src/test/java/com/example/talent_shop/TalentShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talent_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
